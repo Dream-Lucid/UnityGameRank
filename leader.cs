3f28dbd1b9d5a349d8461978c8c0a048557fd3d5
Dream-Lucid <77242212+Dream-Lucid@users.noreply.github.com>
2021-05-26 14:59:50 +0000

    leader.cs

---
+++ leader.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class leader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject scoreCellPrefab;
+    public Button rank;
+
+    public void PaiHangBang()
+    {
+        StartCoroutine(Fenshu.GetHighScore(GetHighScoreCallBack));
+    }
+    public void GetHighScoreCallBack(List<UserData> datas)
+    {
+        int i = 0;
+        foreach (var data in datas)
+        {
+            if (i < 10)
+            {
+                var cell = Instantiate(scoreCellPrefab, transform);
+                cell.GetComponent<rank>().SetModel(data);
+                i++;
+            }
+        }
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
